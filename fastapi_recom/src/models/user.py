54f2251a5e07b33caa764f77175d14fbb2d137e3
Ivlev Alexey <theivlev@yandex.ru>
2025-06-06 16:16:53 +0400

    Убрал поля fio в User

--- fastapi_recom/src/models/user.py
+++ fastapi_recom/src/models/user.py
@@ -8,7 +8,4 @@ from src.models.rating import Ratings
 
 
 class User(Base):
-    name: Mapped[str] = mapped_column(nullable=True)
-    surname: Mapped[str] = mapped_column(nullable=True)
-    patronymic: Mapped[str] = mapped_column(nullable=True)
     ratings: Mapped[List["Ratings"]] = relationship("Ratings", back_populates="user")
