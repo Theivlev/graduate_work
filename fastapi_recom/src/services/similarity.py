270abb8dd1138a74d10f6ac2fd8d27dd39d3fa16
Theivlev <theivlev@yandex.ru>
2025-06-08 20:52:57 +0400

    RecomendationService

--- fastapi_recom/src/services/similarity.py
+++ fastapi_recom/src/services/similarity.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.crud.base import CRUDBase
 from src.db.postgres import get_async_session
 from src.models_ml.user import UserSimilarity
-from src.models.rating import Ratings
+from src.models_ml.film import MovieSimilarity
 
 
 def get_recommendation(session: AsyncSession = Depends(get_async_session)) -> "RecomendationService":
@@ -20,7 +20,7 @@ def get_recommendation(session: AsyncSession = Depends(get_async_session)) -> "R
 class RecomendationService:
     session: AsyncSession
     auth: CRUDBase = CRUDBase(UserSimilarity)
-    rating: CRUDBase = CRUDBase(Ratings)
+    rating: CRUDBase = CRUDBase(MovieSimilarity)
 
     async def get_recommendations():
         pass
