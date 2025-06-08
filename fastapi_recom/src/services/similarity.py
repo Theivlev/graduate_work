09dfc7a6abb6695f67b3d6bee9ffa5e6cea31791
Theivlev <theivlev@yandex.ru>
2025-06-08 20:52:09 +0400

    RecomendationService

---
+++ fastapi_recom/src/services/similarity.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import List
+from uuid import UUID
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.crud.base import CRUDBase
+from src.db.postgres import get_async_session
+from src.models_ml.user import UserSimilarity
+from src.models.rating import Ratings
+
+
+def get_recommendation(session: AsyncSession = Depends(get_async_session)) -> "RecomendationService":
+    """Функция для получения истории входов."""
+    return RecomendationService(session)
+
+
+@dataclass
+class RecomendationService:
+    session: AsyncSession
+    auth: CRUDBase = CRUDBase(UserSimilarity)
+    rating: CRUDBase = CRUDBase(Ratings)
+
+    async def get_recommendations():
+        pass
