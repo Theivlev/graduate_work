625c0798cbee453f7c61611342ac6d4881929703
Theivlev <theivlev@yandex.ru>
2025-06-08 16:20:06 +0400

    Добавил векторы

--- fastapi_recom/src/models_ml/film.py
+++ fastapi_recom/src/models_ml/film.py
@@ -1,3 +1,4 @@
+from sqlalchemy import JSON
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import ForeignKey
 from src.db.postgres import Base
@@ -9,3 +10,9 @@ class MovieSimilarity(Base):
     movie1_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), primary_key=True)
     movie2_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), primary_key=True)
     similarity: Mapped[float] = mapped_column()
+
+
+class MovieVector(Base):
+    """Вектор характеристик фильма"""
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), primary_key=True)
+    vector_data: Mapped[dict] = mapped_column(JSON)
--- fastapi_recom/src/models_ml/user.py
+++ fastapi_recom/src/models_ml/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy import JSON
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import ForeignKey
 from src.db.postgres import Base
@@ -7,3 +8,9 @@ class UserSimilarity(Base):
     user1_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
     user2_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
     similarity: Mapped[float] = mapped_column()
+
+
+class UserVector(Base):
+    """Вектор предпочтений пользователя"""
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    vector_data: Mapped[dict] = mapped_column(JSON)
