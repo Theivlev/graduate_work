4045f38674cc20349109981f8234551277f9955d
Theivlev <theivlev@yandex.ru>
2025-06-08 20:56:20 +0400

    Подкорректировал тип ForeignKey

--- fastapi_recom/src/models_ml/film.py
+++ fastapi_recom/src/models_ml/film.py
@@ -2,17 +2,17 @@ from sqlalchemy import JSON
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import ForeignKey
 from src.db.postgres import Base
-
+from uuid import UUID
 
 class MovieSimilarity(Base):
     """Предвычисленные значения сходства между фильмами."""
 
-    movie1_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), primary_key=True)
-    movie2_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), primary_key=True)
+    movie1_id: Mapped[UUID] = mapped_column(ForeignKey("movies.id"), primary_key=True)
+    movie2_id: Mapped[UUID] = mapped_column(ForeignKey("movies.id"), primary_key=True)
     similarity: Mapped[float] = mapped_column()
 
 
 class MovieVector(Base):
     """Вектор характеристик фильма"""
-    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), primary_key=True)
+    movie_id: Mapped[UUID] = mapped_column(ForeignKey("movies.id"), primary_key=True)
     vector_data: Mapped[dict] = mapped_column(JSON)
--- fastapi_recom/src/models_ml/user.py
+++ fastapi_recom/src/models_ml/user.py
@@ -2,15 +2,16 @@ from sqlalchemy import JSON
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import ForeignKey
 from src.db.postgres import Base
+from uuid import UUID
 
 
 class UserSimilarity(Base):
-    user1_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
-    user2_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    user1_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    user2_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), primary_key=True)
     similarity: Mapped[float] = mapped_column()
 
 
 class UserVector(Base):
     """Вектор предпочтений пользователя"""
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), primary_key=True)
     vector_data: Mapped[dict] = mapped_column(JSON)
