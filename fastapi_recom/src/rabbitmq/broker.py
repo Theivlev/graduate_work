4251efcc53e82b9a2393f95ed8004ec6e529a3d4
Ivlev Alexey <theivlev@yandex.ru>
2025-06-10 17:56:44 +0400

    RABBITMQ_DEFAULT_USER и пароль в env

--- fastapi_recom/src/rabbitmq/broker.py
+++ fastapi_recom/src/rabbitmq/broker.py
@@ -1,5 +1,10 @@
 from faststream.rabbit import RabbitBroker
-from core.config import rabbitmq_settings
+from src.core.config import rabbitmq_settings
+import logging
 
 
-broker = RabbitBroker(rabbitmq_settings.model_config)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+logger.info(f'Данные для подключения {rabbitmq_settings.dsn}')
+broker = RabbitBroker(rabbitmq_settings.dsn)
