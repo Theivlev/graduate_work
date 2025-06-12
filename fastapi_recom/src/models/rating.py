fe5340eea1f06301dfd93eb2a942ec5f8f5dbbf6
Theivlev <theivlev@yandex.ru>
2025-06-12 14:35:32 +0400

    Корректировка импортов и методов Faststream

--- fastapi_recom/src/models/rating.py
+++ fastapi_recom/src/models/rating.py
@@ -6,15 +6,19 @@ from sqlalchemy.orm import mapped_column, Mapped, relationship
 from src.db.postgres import Base
 from src.models.film import Movies
 from src.models.user import Users
+from sqlalchemy import CheckConstraint
 
 
 class Ratings(Base):
     user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
     movie_id: Mapped[UUID] = mapped_column(ForeignKey("movies.id"))
-    rating: Mapped[UUID] = mapped_column(Integer, check_constraint="rating >= 1 AND rating <= 10")
+    rating: Mapped[int] = mapped_column(Integer)
     timestamp: Mapped[datetime.datetime] = mapped_column(
         DateTime(timezone=True),
         default=func.now()
     )
     user: Mapped[Users] = relationship("Users", back_populates="ratings")
     movie: Mapped[Movies] = relationship("Movies", back_populates="ratings")
+    __table_args__ = (
+        CheckConstraint("rating >= 1 AND rating <= 10", name="check_rating_range"),
+    )
